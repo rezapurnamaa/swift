fb6d650faf004708c636296fab446bb99dab12bc
Arief <s0533453@htw-berlin.de>
2016-11-15 15:37:15 +0100

    baume aufbauen

--- SkiFahrer/ViewController.swift
+++ SkiFahrer/ViewController.swift
@@ -11,19 +11,50 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var skiFahrer: UIImageView!
-    
-    
+    let baum = UIImage(named: "baum")
+    var baumView: UIImageView? = nil
+    let screenWidth = UIScreen.main.bounds.size.width
+    var timer: TImer? 
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        baumView = UIImageView(image: baum)
+        
+        if let baume = baumView {
+            view.addSubview(baume)
+            let factor: CGFloat = 0.5
+            var baumeWidth = baume.frame.width
+            var baumeHeight = baume.frame.height
+            baume.frame.origin.x = 50
+            baume.frame.origin.y = 50
+            
+            baumeWidth *= factor
+            baumeHeight *= factor
+            baume.frame.size.width = baumeWidth
+            baume.frame.size.height = baumeHeight         
+        }
     }
 
     @IBAction func moveSlider(_ sender: UISlider) {
         
-        let screenWidth = UIScreen.main.bounds.size.width
         let skiFahrerWidth = skiFahrer.frame.width
         let distance = screenWidth - skiFahrerWidth
         skiFahrer.center.x = CGFloat(sender.value) * distance + skiFahrerWidth/2
+    }
+
+    func moveBaume() {
+
+        guard counter <= 100
+        else {
+            timer?.invalidate()
+            return
+        }
+        if let baume = baumView {
+            baume.frame.originy -= 10
+        }
+        counter += 1
+
+
     }
 
 }
